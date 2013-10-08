b14114554dc59598d1196866d9c355609163656b
thomazpadilha <padilha.thomaz@gmail.com>
2013-10-08 00:39:30 -0300

    Hadouken!

--- Assets/ScriptTeste.cs
+++ Assets/ScriptTeste.cs
@@ -5,7 +5,9 @@ public class ScriptTeste : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//Faz uns bagulhos muito loucos, da umas piruetas marotas e solta hadouken!
+		Instantiate(testePrefab);
+		//Hadouken!
 	}
 	
 	// Update is called once per frame
